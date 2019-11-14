523bb498e3864a58ec9f8630ef7d69ef54590525
Pradeep Chinchole <pradeepch82@gmail.com>
2019-11-14 23:20:21 +0530

    Update EmployeeReposiotry.java

--- spring-boot-ems/src/main/java/com/atossyntel/ems/reposiotry/EmployeeReposiotry.java
+++ spring-boot-ems/src/main/java/com/atossyntel/ems/reposiotry/EmployeeReposiotry.java
@@ -1,11 +1,12 @@
 package com.atossyntel.ems.reposiotry;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.atossyntel.ems.model.Employee;
 
 
 @Repository("employeeReposiotry")
-public interface EmployeeReposiotry extends  JPARepository<Employee, Integer> {
+public interface EmployeeReposiotry extends  JpaRepository<Employee, Integer> {
 
 }
